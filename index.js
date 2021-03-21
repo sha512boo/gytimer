425228ff905a5df5b7def42617c6095180afeca8
=sha512boo <bargontaba@gmail.com>
2021-03-21 19:59:39 +1000

    add async sleep function

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 
 
-const timeoutEventPool = []
+const timeoutEventPool = [] 
 
-module.exports = {
-    setCustomTimeout: (func, duration = 0) => {
-        if (isNaN(duration) || duration < 0) duration = 0
-        let startTimeout = Date.now()
-        let timeout = setInterval(() => {
-            try {
-                if (Date.now() >= startTimeout + duration) {
-                    func()
-                    clearInterval(timeout)
-                }
-            } catch (err) {
-                console.log(err)
+function setCustomTimeout (func, duration = 0) {
+    if (isNaN(duration) || duration < 0) duration = 0
+    let startTimeout = Date.now()
+    let timeout = setInterval(() => {
+        try {
+            if (Date.now() >= startTimeout + duration) {
+                func()
+                clearInterval(timeout)
             }
-        })
-        timeout.id = getGeneratedTimeoutID()
-        timeoutEventPool.push(timeout)
-        return timeout
-    }, 
-    clearCustomTimeout: (object) => {
-        let timeout = getTimeoutByID(object.id)
-        if (timeout) {
-            clearInterval(timeout)
-            timeoutEventPool.splice(timeoutEventPool.indexOf(timeout), 1)
+        } catch (err) {
+            console.log(err)
         }
-    },
-    isCustomTimeoutExists: (object) => {
-        let result = !!getTimeoutByID(object.id) 
-        return result
+    })
+    timeout.id = getGeneratedTimeoutID()
+    timeoutEventPool.push(timeout)
+    return timeout
+} 
+function clearCustomTimeout (object) {
+    let timeout = getTimeoutByID(object.id)
+    if (timeout) {
+        clearInterval(timeout)
+        timeoutEventPool.splice(timeoutEventPool.indexOf(timeout), 1)
     }
 }
-
-const getTimeoutByID = (id) => {
+function isCustomTimeoutExists (object) {
+    let result = !!getTimeoutByID(object.id) 
+    return result
+}
+function getTimeoutByID (id) {
 	let result = null
     for (let key in timeoutEventPool) {
         if (timeoutEventPool[key].id == id) {
@@ -42,19 +39,32 @@ const getTimeoutByID = (id) => {
         }
     }
 	return result
-}
-
-const getGeneratedTimeoutID = () => {
+} 
+function getGeneratedTimeoutID () {
 	let result = null
 	while (!result) {
-		result = getRandomInteger(1, 100000)
+		result = getRandomInteger(1, Number.MAX_SAFE_INTEGER)
 		if (!!getTimeoutByID(result)) result = null
 	}
 	return result
-}
-
-const getRandomInteger = (min, max) => {
+} 
+function getRandomInteger (min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1)
     rand = Math.round(rand);
     return rand;
+}
+
+async function sleep (duration) {
+    return new Promise((resolve) => {
+        setCustomTimeout(() => {
+            resolve(true)
+        }, duration)
+    })
+}
+
+module.exports = {
+    setCustomTimeout,
+    clearCustomTimeout,
+    isCustomTimeoutExists,
+    sleep
 }
