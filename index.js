f9920d92588638be5143c0579dc09f1db4898783
=sha512boo <bargontaba@gmail.com>
2021-07-02 18:19:15 +1000

    fix bugs v2

--- index.js
+++ index.js
@@ -3,29 +3,37 @@
 const timeoutEventPool = [] 
 
 function setCustomTimeout (func, duration = 0) {
-    if (isNaN(duration) || duration < 0) duration = 0
-    let startTimeout = Date.now()
-    let timeout = {}
-    timeout.id = getGeneratedTimeoutID()
-    timeout.interval = setInterval(() => {
-        try {
-            if (Date.now() >= startTimeout + duration) {
-                func()
+    try {
+        if (isNaN(duration) || duration < 0) duration = 0
+        let startTimeout = Date.now()
+        let timeout = {} 
+        timeout.id = getGeneratedTimeoutID()
+        timeout.interval = setInterval(() => {
+            try {
+                if (Date.now() >= startTimeout + duration) {
+                    func()
+                    clearCustomTimeout(timeout)
+                }
+            } catch (err) {
                 clearCustomTimeout(timeout)
+                console.log(err, `timeout id: ${timeout.id}`)
             }
-        } catch (err) {
-            clearCustomTimeout(timeout)
-            console.log(err, `timeout id: ${timeout.id}`)
-        }
-    })
-    timeoutEventPool.push(timeout)
-    return timeout
+        })
+        timeoutEventPool.push(timeout)
+        return timeout
+    } catch (err) {
+        console.log(err)
+    }
 } 
 function clearCustomTimeout (object) {
-    let timeout = getTimeoutByID(object.id)
-    if (timeout) {
-        clearInterval(timeout.inverval)
-        timeoutEventPool.splice(timeoutEventPool.indexOf(timeout), 1)
+    try {
+        let timeout = getTimeoutByID(object.id)
+        if (timeout) { 
+            clearInterval(timeout.interval)
+            timeoutEventPool.splice(timeoutEventPool.indexOf(timeout), 1)
+        }
+    } catch (err) {
+        console.log(err)
     }
 }
 function isCustomTimeoutExists (object) {
